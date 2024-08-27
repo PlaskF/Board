98fe1b7901d87a747562c5b44a300f4cc190c6e7
PaksF <flaksfjtm@gmail.com>
2024-08-27 15:33:51 +0900

    변경

---
+++ src/main/java/kr/ac/kopo/board/entity/Member.java
@@ -0,0 +1,14 @@
+package kr.ac.kopo.board.entity;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class Member extends BaseEntity{
+
+}
